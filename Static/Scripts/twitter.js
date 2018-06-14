0029778016c5b2a00ff589824300f6920942f54c
maqalexh <30702470+maqalexh@users.noreply.github.com>
2018-06-13 18:01:17 -0700

    Add Static to fix Catsone Compatability

---
+++ Static/Scripts/twitter.js
@@ -0,0 +1,23 @@
+﻿/*jslint plusplus: true */
+function formatNews() {
+    "use strict";
+    try {
+        var iLength = $('.e-entry-title').length, iCount, value, href;
+        if (0 === iLength) {
+            window.setTimeout(formatNews, 100);
+        }
+        for (iCount = 0; iCount < iLength; iCount++) {
+            value = $('.e-entry-title:nth(' + iCount + ')').html();
+            console.log(value);
+            value = value.substring(0, value.indexOf('<a '));
+            href = $('.e-entry-title:nth(' + iCount + ') a').attr('href');
+            if (value) {
+                $('.e-entry-title:nth(' + iCount + ')').html('<a class="newsContent" href="' + href + '"> ' + value + ' </a>');
+            }
+        }
+        $('.SandboxRoot').show();
+    } catch (ex) {
+        setTimeout(formatNews, 100);
+    }
+}
+formatNews();
