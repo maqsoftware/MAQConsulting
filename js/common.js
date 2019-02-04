4e2c486ff6c3af7ae03f042f75ef0e539c2f9efc
Siddhesh Hindalekar <v-sihind@microsoft.com>
2019-02-04 10:02:45 +0530

    disabled ordering

--- js/common.js
+++ js/common.js
@@ -333,44 +333,44 @@ function showMap(Latitude, Longitude, address) {
         //label: 'Hello World!',
         draggable: true
     });
-    //var infowindow = new google.maps.InfoWindow({
-    //    content: '<h5 Style="text-transform: none;">MAQ Software</h5>' + address
-    //});
-    //google.maps.event.addListener(oMarker, 'click', function () {
-    //    infowindow.open(map, oMarker);
-    //});
-    //infowindow.open(map, oMarker);
+    var infowindow = new google.maps.InfoWindow({
+        content: '<h5 Style="text-transform: none;">MAQ Software</h5>' + address
+    });
+    google.maps.event.addListener(oMarker, 'click', function () {
+        infowindow.open(map, oMarker);
+    });
+    infowindow.open(map, oMarker);
 
-    //google.maps.Marker.prototype.setLabel = function (label) {
-    //    this.label = new MarkerLabel({
-    //        map: this.map,
-    //        marker: this,
-    //        text: label
-    //    });
-    //    this.label.bindTo('position', this, 'position');
-    //};
+    google.maps.Marker.prototype.setLabel = function (label) {
+        this.label = new MarkerLabel({
+            map: this.map,
+            marker: this,
+            text: label
+        });
+        this.label.bindTo('position', this, 'position');
+    };
 
-    //var MarkerLabel = function (options) {
-    //    this.setValues(options);
-    //    this.span = document.createElement('span');
-    //    this.span.className = 'map-marker-label';
-    //};
+    var MarkerLabel = function (options) {
+        this.setValues(options);
+        this.span = document.createElement('span');
+        this.span.className = 'map-marker-label';
+    };
 
-    //MarkerLabel.prototype = $.extend(new google.maps.OverlayView(), {
-    //    onAdd: function () {
-    //        this.getPanes().overlayImage.appendChild(this.span);
-    //        var self = this;
-    //        this.listeners = [
-    //        google.maps.event.addListener(this, 'position_changed', function () { self.draw(); })];
-    //    },
-    //    draw: function () {
-    //        var text = String(this.get('text'));
-    //        var position = this.getProjection().fromLatLngToDivPixel(this.get('position'));
-    //        this.span.innerHTML = text;
-    //        this.span.style.left = (position.x - (markerSize.x / 2)) - (text.length * 3) + 10 + 'px';
-    //        this.span.style.top = (position.y - markerSize.y + 40) + 'px';
-    //    }
-    //});
+    MarkerLabel.prototype = $.extend(new google.maps.OverlayView(), {
+        onAdd: function () {
+            this.getPanes().overlayImage.appendChild(this.span);
+            var self = this;
+            this.listeners = [
+            google.maps.event.addListener(this, 'position_changed', function () { self.draw(); })];
+        },
+        draw: function () {
+            var text = String(this.get('text'));
+            var position = this.getProjection().fromLatLngToDivPixel(this.get('position'));
+            this.span.innerHTML = text;
+            this.span.style.left = (position.x - (markerSize.x / 2)) - (text.length * 3) + 10 + 'px';
+            this.span.style.top = (position.y - markerSize.y + 40) + 'px';
+        }
+    });
 }
 
 function JSON_CALLBACK() {
@@ -698,7 +698,7 @@ $(document).ready(function () {
            }
            $("#tbdy").append(table);
            $('#dttable').DataTable({
-               "order": [[0, 'desc']]
+              "ordering": false
            });
            $('#jobDescriptionContainer').hide();
            $(".loadingIcon").hide();
